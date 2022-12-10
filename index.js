5f065092480246896e16eec12d58f569f70288fd
saharabanu <saharabanungn@gmail.com>
2022-12-10 23:51:15 +0600

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -11,7 +12,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ourphyd.mongodb.net/Blog_Website?retryWrites=true&w=majority`;
-console.log(uri)
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -20,50 +21,66 @@ async function run() {
         const db = client.db("blog_content");
         const blogCollection = db.collection("blog");
     
+        // get all blogs api
         app.get("/blogs", async (req, res) => {
           const cursor = blogCollection.find({});
           const blog = await cursor.toArray();
     
           res.send({ status: true, data: blog });
         });
-    
+        
+        //  get single blog api 
+        app.get('/blog/:id',async(req,res)=>{
+          const id = req.params.id;
+          const query = { _id:ObjectId(id) };
+          const blog = await blogCollection.findOne(query);
+          // console.log(blog);
+          res.json(blog);
+
+          });
+
+
+
+      // post  blog api
         app.post("/blog", async (req, res) => {
           const blog = req.body;
-    
           const result = await blogCollection.insertOne(blog);
           // console.log(product);
     
           res.send(result);
         });
-    
+       // delete  blog api
         app.delete("/blog/:id", async (req, res) => {
           const id = req.params.id;
-    
           const result = await blogCollection.deleteOne({ _id: ObjectId(id) });
           res.send(result);
         });
-        // app.put("/blog/:id", async (req, res) => {
-        //   const id = req.params.id;
-        //   const updatedBlog = req.body;
-        //   const filter = { _id: ObjectId(id) };
-        //   const options = { upsert: true };
-        //   const updateDoc = {
-        //     $set: {
-        //       model: updatedBlog.model,
-        //       image: updatedBlog.image,
-        //       status: updatedBlog.status,
-        //       keyFeature: updatedBlog.keyFeature,
-        //       price: updatedBlog.price,
-        //       rating: updatedBlog.rating,
-        //       spec: updatedBlog.spec,
-              
-        //     },
-        //   };
+
+          // edit blog api
+        app.put("/blog/:id", async (req, res) => {
+          const id = req.params.id;
+          const updatedBlog = req.body;
+          const filter = { _id: ObjectId(id) };
+          const options = { upsert: true };
+          const updateDoc = {
+            $set: {
+              title: updatedBlog.title,
+              image: updatedBlog.image,
+              category: updatedBlog.category,
+              author: updatedBlog.author,
+              desc1: updatedBlog.desc1,
+              desc2: updatedBlog.desc2,
+              desc3: updatedBlog.desc3,
+
+            },
+          };
     
-        //   const result = await blogCollection.updateOne(filter, updateDoc, options);
-        //   console.log(result);
-        //   res.send(result);
-        // });
+
+          const result = await blogCollection.updateOne(filter, updateDoc, options);
+          console.log(result);
+          res.send(result);
+        });
+
       }
     finally {
       // Ensures that the client will close when you finish/error
